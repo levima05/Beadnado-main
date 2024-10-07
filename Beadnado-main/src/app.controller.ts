67f1f354adc01de6d6b6d43f0a20645b777845c3
Petrik Diak <diak@petrik.hu>
2024-10-07 13:23:02 +0200

    second

---
+++ Beadnado-main/src/app.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Post, Redirect, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('index')
+  getHello() {
+    return {
+      message: this.appService.getHello()
+    };
+  }
+
+  @Post('rendeles')
+  @Redirect('/rendeles')
+  getIndex() {
+
+  }
+
+  @Get('rendeles')
+  @Render('rendeles')
+  getRendeles() {
+
+  }
+}
